fc9f20918a755ef9f0676adc50861ac60ee8b6b7
MAURICIO JESUS ARIAS BONILLA <mauricio.arias@ulatina.net>
2025-11-22 01:01:15 -0600

    Feat: Configure bot intents for enhanced message and member management

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,7 +1,14 @@
 from discord.ext import commands
+import discord
 
-# Inicializar el bot
-bot = commands.Bot(command_prefix="!")
+# Configurar intents
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.members = True
+
+# Inicializar el bot con intents
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Exportar el objeto bot
 __all__ = ["bot"]
