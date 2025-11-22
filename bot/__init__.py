49dba1a9befd72eb090125f2feee696b3bd3e971
MAURICIO JESUS ARIAS BONILLA <mauricio.arias@ulatina.net>
2025-11-22 01:13:31 -0600

    Feat: Load GameRoles cog and synchronize commands on bot ready event

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+from bot.cogs.game_roles import GameRoles
 
 # Configurar intents
 intents = discord.Intents.default()
@@ -10,5 +11,14 @@ intents.members = True
 # Inicializar el bot con intents
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Cargar cogs
+bot.add_cog(GameRoles(bot))
+
 # Exportar el objeto bot
 __all__ = ["bot"]
+
+# Sincronizar comandos
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"Bot listo como {bot.user}")
